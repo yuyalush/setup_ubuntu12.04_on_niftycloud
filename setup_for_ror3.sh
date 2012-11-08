eb9ef0236a81a48bc5bfd0d775b5fcff78c2fdc7
Yuya Yoshida <yoshidayuya@gmail.com>
2012-11-08 14:41:01 +0900

    no message

--- setup_for_ror3.sh
+++ setup_for_ror3.sh
@@ -41,6 +41,39 @@ rbenv rehash
 echo "Rails finished." >> log.txt
 date >> log.txt
 
+# Sqlite3
+echo "Sqlite3 start." >> log.txt
+date >> log.txt
+wget http://www.sqlite.org/sqlite-autoconf-3071401.tar.gz
+tar zxf sqlite-autoconf-3071401.tar.gz
+cd sqlite-autoconf-3071401/
+./configure
+make -j
+make install
+cd ..
+gem install sqlite3-ruby
+echo "Sqlite3 finished." >> log.txt
+date >> log.txt
+
+# Check
+echo "ruby & rails check" >> log.txt
+ruby -v >> log.txt
+rails -v >> log.txt
+
+# Test Rails App
+rails new testapp --skip-bundle
+cd testapp
+#echo "gem 'therubyracer'" >> Gemfile
+echo "gem 'unicorn'" >> Gemfile
+echo "gem 'nifty-generators'" >> Gemfile
+echo "gem 'mocha'" >> Gemfile
+bundle install
+rails g nifty:scaffold Book title:string price:integer
+rails g nifty:layout -f
+rake db:migrate
+bundle exec unicorn_rails -D
+ufw allow 8080
+
 echo "Finish!!!!!" >> ../log.txt
 date >> ../log.txt
 cat ../log.txt
